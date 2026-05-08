4da9f74d89cf05bb6e00dab9c5ba12ea353a6b59
Orion Rawlings <orionrawlings@gmail.com>
2026-05-07 18:45:34 -0600

    Finished class constructor.

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -14,7 +14,9 @@ public class ChessGame {
     private TeamColor teamTurn;
 
     public ChessGame() {
-
+        board = new ChessBoard();
+        board.resetBoard();
+        teamTurn = TeamColor.WHITE;
     }
 
     /**
