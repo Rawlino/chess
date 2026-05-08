6b5d09e761afa55afa7a36811bab3f04b4f03fa2
Orion Rawlings <orionrawlings@gmail.com>
2026-05-07 18:43:44 -0600

    Finished getter and setter methods.

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -21,7 +21,7 @@ public class ChessGame {
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-
+        return teamTurn;
     }
 
     /**
@@ -30,7 +30,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-
+        teamTurn = team;
     }
 
     /**
