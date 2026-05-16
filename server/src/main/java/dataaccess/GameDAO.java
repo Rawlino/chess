32deb59d44fb104719d932be7d45f9ae3b2bf5b9
Orion Rawlings <orionrawlings@gmail.com>
2026-05-16 04:19:16 -0600

    Filled out DAOs with basic Data Access Methods (maybe more later)

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,16 @@
 package dataaccess;
 
+import model.GameData;
+
 public interface GameDAO {
+
+    GameData createGame() throws DataAccessException;
+
+    GameData getGame(int gameID) throws DataAccessException;
+
+    void listGames() throws DataAccessException;
+
+    void updateGame(int gameID) throws DataAccessException;
+
+    void clear() throws DataAccessException;
 }
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,4 +1,12 @@
 package dataaccess;
 
+import model.UserData;
+
 public interface UserDAO {
+
+    UserData createUser() throws DataAccessException;
+
+    UserData getUser(String username) throws DataAccessException;
+
+    void clear() throws DataAccessException;
 }
