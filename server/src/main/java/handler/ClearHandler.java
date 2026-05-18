fa0baaddd7c7c2625a7bd0ad24cef4b7e766a532
Orion Rawlings <orionrawlings@gmail.com>
2026-05-18 16:46:17 -0600

    Finished Clear Handler, moving onto implementing it into server to finish first endpoint logic.

--- server/src/main/java/handler/ClearHandler.java
+++ server/src/main/java/handler/ClearHandler.java
@@ -1,4 +1,20 @@
 package handler;
 
+import io.javalin.http.Context;
+import dataaccess.DataAccessException;
+import service.ClearService;
+
 public class ClearHandler {
+
+    private final ClearService clearService;
+
+    ClearHandler(ClearService clearService) {
+        this.clearService = clearService;
+    }
+
+    private void clearDB(Context ctx) throws DataAccessException {
+        clearService.clearDB();
+        ctx.status(200);
+    }
+
 }
