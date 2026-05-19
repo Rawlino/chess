e49bd9c8ba848d44dcf4322dba726a87f007117a
Orion Rawlings <orionrawlings@gmail.com>
2026-05-19 12:21:34 -0600

    Fixed GameHandler and passed WAY more tests, still 9 to work on but in a much better place.

--- server/src/main/java/handler/GameHandler.java
+++ server/src/main/java/handler/GameHandler.java
@@ -1,6 +1,8 @@
 package handler;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import io.javalin.http.Context;
 import dataaccess.DataAccessException;
 import model.AuthData;
@@ -19,20 +21,20 @@ public class GameHandler {
 
     public void listGames(Context ctx) throws DataAccessException {
         Collection<GameData> games = gameService.listGames(ctx.header("Authorization"));
-        ctx.result(new Gson().toJson(games));
+        String response = String.format("{\"games\": %s}", new Gson().toJson(games));
+        ctx.result(response);
     }
 
     public void createGame(Context ctx) throws DataAccessException {
         GameData game = new Gson().fromJson(ctx.body(), GameData.class);
-        AuthData auth = new Gson().fromJson(ctx.body(), AuthData.class);
-        gameService.createGame(auth.authToken(), game.gameName());
-        ctx.result(new Gson().toJson(game.gameID()));
+        int gameID = gameService.createGame(ctx.header("Authorization"), game.gameName());
+        String response = String.format("{\"gameID\": %d}", gameID);
+        ctx.result(response);
     }
 
     public void joinGame(Context ctx) throws DataAccessException {
-        GameData game = new Gson().fromJson(ctx.body(), GameData.class);
-        AuthData auth = new Gson().fromJson(ctx.body(), AuthData.class);
-        gameService.joinGame(auth.authToken(), auth.username(), game.gameID());
+        JsonObject obj = JsonParser.parseString(ctx.body()).getAsJsonObject();
+        gameService.joinGame(ctx.header("Authorization"), obj.get("playerColor").getAsString(), obj.get("gameID").getAsInt());
         ctx.result("{}");
     }
 
