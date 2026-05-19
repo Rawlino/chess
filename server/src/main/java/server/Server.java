8bbd3978ccd8ebe3a599ea24149c89783070806e
Orion Rawlings <orionrawlings@gmail.com>
2026-05-18 22:28:11 -0600

    Added user endpoints and exception method to Server.java.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -12,16 +12,34 @@ public class Server {
 
     private final Javalin javalin;
     private final ClearHandler clearHandler;
+    private final UserHandler userHandler;
+    private final GameHandler gameHandler;
 
     public Server() {
 
         clearHandler = new ClearHandler(new ClearService(new MemoryUserDAO(), new MemoryGameDAO(), new MemoryAuthDAO()));
+        userHandler = new UserHandler(new UserService(new MemoryAuthDAO(), new MemoryUserDAO()));
+        gameHandler = new GameHandler(new GameService(new MemoryGameDAO(), new MemoryAuthDAO()));
 
         javalin = Javalin.create(config -> config.staticFiles.add("web"))
         // Register your endpoints and exception handlers here.
-//                .post("/")
-                .delete("/db", this.clearHandler::clearDB);
+                .post("/user", this.userHandler::register)
+                .post("/session", this.userHandler::login)
+                .delete("/session", this.userHandler::logout)
+                .delete("/db", this.clearHandler::clearDB)
+                .get("/game", this.gameHandler::l)
+                .exception(DataAccessException.class, this::exceptionHandler);
 
+    }
+
+    private void exceptionHandler(DataAccessException ex, Context ctx) {
+        if (ex.toString().contains("unauthorized")) {
+            ctx.status(401);
+        } else if (ex.toString().contains("bad response")) {
+            ctx.status(400);
+        } else if (ex.toString().contains("already taken")) {
+            ctx.status(403);
+        }
     }
 
     public int run(int desiredPort) {
