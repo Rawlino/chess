0827e259a6bbdf8d9dab75e3d9f3a32c0a4b1417
Orion Rawlings <orionrawlings@gmail.com>
2026-05-20 13:31:01 -0600

    Finished ClearServiceTest

--- server/src/test/java/service/ClearServiceTest.java
+++ server/src/test/java/service/ClearServiceTest.java
@@ -1,5 +1,9 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,7 +11,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class ClearServiceTest {
 
     @Test
-    void clearDB() {
+    void clearDBPositive() throws DataAccessException {
+        MemoryUserDAO memoryUserDAO = new MemoryUserDAO();
+        MemoryGameDAO memoryGameDAO = new MemoryGameDAO();
+        MemoryAuthDAO memoryAuthDAO = new MemoryAuthDAO();
+        ClearService clearService = new ClearService(memoryUserDAO, memoryGameDAO, memoryAuthDAO);
+        UserService userService = new UserService(memoryAuthDAO, memoryUserDAO);
+
+        userService.register("butt", "butt", "butt@mail");
+
+        clearService.clearDB();
+
+        assertNull(memoryUserDAO.getUser("butt"));
     }
 
 }
