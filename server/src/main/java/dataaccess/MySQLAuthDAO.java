7eb2185dc93efc383e0823fee1b8ac3b22b8394b
Orion Rawlings <orionrawlings@gmail.com>
2026-05-23 23:30:16 -0600

    Was able to create an SQL auth table and view it in mysql. Moving onto implementing methods on Monday.

--- server/src/main/java/dataaccess/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -1,4 +1,84 @@
 package dataaccess;
 
-public class MySQLAuthDAO {
+import java.sql.Connection;
+import com.google.gson.Gson;
+import model.*;
+
+import java.sql.*;
+
+import static dataaccess.DatabaseManager.createDatabase;
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+import dataaccess.DataAccessException;
+
+public class MySQLAuthDAO implements AuthDAO {
+
+    public MySQLAuthDAO() throws DataAccessException {
+        configureDatabase();
+    }
+
+    public AuthData createAuth(String authToken, String username) throws DataAccessException {
+        throw new DataAccessException("Not implemented");
+    }
+
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        throw new DataAccessException("Not implemented");
+    }
+
+    public void deleteAuth(String authToken) throws DataAccessException {
+
+    }
+
+    public void clear() throws DataAccessException {
+
+    }
+
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (int i = 0; i < params.length; i++) {
+                    Object param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  auth (
+              `authToken` VARCHAR(255) NOT NULL,
+              `username` VARCHAR(255) NOT NULL,
+              PRIMARY KEY (`authToken`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+
+    private void configureDatabase() throws DataAccessException {
+        createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+
 }
--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -1,9 +1,18 @@
 package server;
 
+import dataaccess.DataAccessException;
+import dataaccess.MySQLAuthDAO;
+
 public class ServerMain {
     public static void main(String[] args) {
         Server server = new Server();
         server.run(8080);
+        try {
+            new MySQLAuthDAO();
+        } catch (DataAccessException ex) {
+            ex.printStackTrace();
+        }
+
 
         System.out.println("♕ 240 Chess Server");
     }
