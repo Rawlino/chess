74ac88a9b4c01fbc69145e30d7d82069e9a255a8
Orion Rawlings <orionrawlings@gmail.com>
2026-06-01 12:42:07 -0600

    Edited ClientMain in preparation for switching between REPLs

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,6 +1,7 @@
 package client;
 
 import chess.*;
+import repl.LoggedInREPL;
 import repl.LoggedOutREPL;
 import server.ServerFacade;
 
@@ -8,13 +9,21 @@ public class ClientMain {
     public static void main(String[] args) {
         String serverUrl = "http://localhost:8080";
         ServerFacade sharedServerFacade = new ServerFacade(serverUrl);
+        String result = "LOGGED_OUT";
         if (args.length == 1) {
             serverUrl = args[0];
         }
 
         try {
-            new LoggedOutREPL(sharedServerFacade).run();
-
+            while (!result.equals("QUIT")) {
+                if (result.equals("LOGGED_OUT")) {
+                    result = new LoggedOutREPL(sharedServerFacade).run();
+                } else if (result.equals("LOGGED_IN")) {
+                    result = new LoggedInREPL(sharedServerFacade).run();
+                } else if (result.equals("IN_GAME")) {
+                    result = new LoggedInREPL(sharedServerFacade).run();
+                }
+            }
         } catch (Throwable ex) {
             System.out.printf("Unable to start server: %s%n", ex.getMessage());
         }
