d0ec164ab14bdc022c4f58e3b4c4e11fb87c3fcc
Orion Rawlings <orionrawlings@gmail.com>
2026-06-01 22:29:40 -0600

    Edited ClientMain to create single REPLs and switch between those rather than creating new ones everytime.

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -10,19 +10,21 @@ public class ClientMain {
     public static void main(String[] args) {
         String serverUrl = "http://localhost:8080";
         ServerFacade sharedServerFacade = new ServerFacade(serverUrl);
+        LoggedOutREPL loggedOutREPL = new LoggedOutREPL(sharedServerFacade);
+        LoggedInREPL loggedInREPL = new LoggedInREPL(sharedServerFacade);
+        InGameREPL inGameREPL = new InGameREPL(sharedServerFacade);
         String result = "LOGGED_OUT";
         if (args.length == 1) {
             serverUrl = args[0];
         }
-
         try {
             while (!result.equals("QUIT")) {
                 if (result.equals("LOGGED_OUT")) {
-                    result = new LoggedOutREPL(sharedServerFacade).run();
+                    result = loggedOutREPL.run();
                 } else if (result.equals("LOGGED_IN")) {
-                    result = new LoggedInREPL(sharedServerFacade).run();
+                    result = loggedInREPL.run();
                 } else if (result.equals("IN_GAME")) {
-                    result = new InGameREPL(sharedServerFacade).run();
+                    result = inGameREPL.run();
                 }
             }
         } catch (Throwable ex) {
